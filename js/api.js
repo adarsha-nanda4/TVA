7830153b5ad689d8f67e1199f6c3dfe132640ce3
Adarsh Nanda <adarshananda51@gmail.com>
2024-08-18 00:37:48 +0530

    ...

--- js/api.js
+++ js/api.js
@@ -77,29 +77,31 @@ fetch('https://shiksha-aa.vercel.app/api/routine/')
 
 
     self.addEventListener('install', (event) => {
-        event.waitUntil(
-          caches.open('my-cache').then((cache) => {
-            return cache.addAll([
-              '/',
-              '/index.html',
-              '/styles.css',
-              '/script.js',
-              '/manifest.json',
-              '/icon-192x192.png',
-              '/icon-512x512.png'
-            ]);
-          })
-        );
-      });
+      event.waitUntil(
+        caches.open('my-cache').then((cache) => {
+          return cache.addAll([
+            '/',
+            '/index.html',
+            '/styles.css',
+            '/script.js',
+            '/offline.html', // Add the offline page to the cache
+            '/app.json',
+            '/icon-192x192.png',
+            '/icon-512x512.png'
+          ]);
+        })
+      );
+    });
+    
       
-      self.addEventListener('fetch', (event) => {
-        event.respondWith(
-          caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-          })
-        );
-      });
-      
+    self.addEventListener('fetch', (event) => {
+      event.respondWith(
+        fetch(event.request).catch(() => {
+          return caches.match('/offline.html');
+        })
+      );
+    });
+    
     
       if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
