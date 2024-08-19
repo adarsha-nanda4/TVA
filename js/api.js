e457fce69b018bd87f3b09a17af584dfa8821691
Adarsh Nanda <adarshananda51@gmail.com>
2024-08-19 10:43:44 +0530

    ..

--- js/api.js
+++ js/api.js
@@ -126,3 +126,13 @@ fetch('https://shiksha-aa.vercel.app/api/routine/')
           });
         });
       };
+
+
+
+
+
+      if (!navigator.serviceWorker.controller) {
+        navigator.serviceWorker.register("/sw.js").then(function(reg) {
+            console.log("Service worker has been registered for scope: " + reg.scope);
+        });
+    }
