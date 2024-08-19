3061ea41eb6d701edb89b94cc67968b77e69b35b
Adarsh Nanda <adarshananda51@gmail.com>
2024-08-19 21:55:54 +0530

    //

--- sw.js
+++ sw.js
@@ -2,13 +2,13 @@ self.addEventListener("install", function(event) {
     event.waitUntil(preLoad());
   });
   
-  var preLoad = function(){
-    console.log("Installing web app");
-    return caches.open("offline").then(function(cache) {
-      console.log("caching index and important routes");
-      return cache.addAll(["/offline.html"]);
-    });
-  };
+  // var preLoad = function(){
+  //   console.log("Installing web app");
+  //   return caches.open("offline").then(function(cache) {
+  //     console.log("caching index and important routes");
+  //     return cache.addAll(["/offline.html"]);
+  //   });
+  // };
   
   self.addEventListener("fetch", function(event) {
     event.respondWith(checkResponse(event.request).catch(function() {
