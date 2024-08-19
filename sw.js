a35198c97ac4000062f8c1c0b15ffd580048ab6f
Adarsh Nanda <adarshananda51@gmail.com>
2024-08-19 22:47:45 +0530

    ..

--- sw.js
+++ sw.js
@@ -1,35 +1,57 @@
 self.addEventListener("install", function(event) {
-    event.waitUntil(preLoad());
+  event.waitUntil(preLoad());
+});
+
+var preLoad = function() {
+  console.log("Installing web app");
+  return caches.open("offline").then(function(cache) {
+    console.log("caching index and important routes");
+    return cache.addAll(["/offline.html", "/offline.gif"]);
   });
-  
-  var preLoad = function(){
-    console.log("Installing web app");
-    return caches.open("offline").then(function(cache) {
-      console.log("caching index and important routes");
-      return cache.addAll(["/offline.html"]);
-      return cache.addAll(["/offline.gif"]);
-    });
-  };
-  
-  self.addEventListener("fetch", function(event) {
-    event.respondWith(checkResponse(event.request).catch(function() {
+};
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    checkResponse(event.request).catch(function() {
       return returnFromCache(event.request);
-    }));
-    event.waitUntil(addToCache(event.request));
+    })
+  );
+  event.waitUntil(
+    addToCache(event.request)
+  );
+});
+
+var checkResponse = function(request) {
+  return fetch(request).then(function(response) {
+    if (response.status === 404) {
+      return Promise.reject("Not found");
+    }
+    return response;
   });
-  
-  var checkResponse = function(request){
-    return new Promise(function(fulfill, reject) {
-      fetch(request).then(function(response){
-        if(response.status !== 404) {
-          fulfill(response);
-        } else {
-          reject();
-        }
-      }, reject);
+};
+
+var returnFromCache = function(request) {
+  return caches.open("offline").then(function(cache) {
+    return cache.match(request).then(function(matching) {
+      return matching || cache.match("/offline.html");
     });
-  };
-  
+  });
+};
+
+var addToCache = function(request) {
+  return caches.open("offline").then(function(cache) {
+    return fetch(request).then(function(response) {
+      if (response.status === 200) {
+        cache.put(request, response.clone());
+      }
+      return response;
+    });
+  });
+};
+
+
+
+
   // var addToCache = function(request){
   //   return caches.open("offline").then(function (cache) {
   //     return fetch(request).then(function (response) {
@@ -39,20 +61,3 @@ self.addEventListener("install", function(event) {
   //   });
   // };
   
-  var returnFromCache = function(request){
-    return caches.open("offline").then(function (cache) {
-      return cache.match(request).then(function (matching) {
-       if(!matching || matching.status == 404) {
-         return cache.match("offline.html");
-         return cache.addAll(["/offline.gif"]);
-       } else {
-         return matching;
-       }
-      });
-    });
-  };
-
-
-
-
-
