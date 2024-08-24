6b346920f910fb1a0f5927ba29f5f136b34569ae
Adarsh Nanda <adarshananda51@gmail.com>
2024-08-24 16:48:39 +0530

    ...

--- js/login.js
+++ js/login.js
@@ -6,8 +6,8 @@ const btn = document.getElementById("loginBtn");
 userName.value=""
 password.value=""
 
-const emailid= "smita"
-const pass="smita"
+const emailid= "abcd"
+const pass="abcd"
 
 
 btn.addEventListener("click",function(){
@@ -16,7 +16,7 @@ btn.addEventListener("click",function(){
       btn.innerHTML="please wait..."
    
       setTimeout(function() {
-       window.location=("https://siksha.vercel.app/page/just.html")
+       window.location=("https://siksha.vercel.app")
       },1000);
    }
 })
