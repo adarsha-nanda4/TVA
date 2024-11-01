2aadc847929416087030058858c85236b53d1882
Adarsh Nanda <adarshananda51@gmail.com>
2024-11-01 23:42:18 +0530

    //

--- js/timesedul.js
+++ js/timesedul.js
@@ -71,7 +71,7 @@ function updateTime() {
     else if (hours === 9) { setActiveClass(classTwo, activeTimeTwo); }
     else if (hours === 10) { setActiveClass(classThree, activeTimeThree); }
     else if (hours === 11) { setActiveClass(classFour, activeTimeFour); }
-    else if (hours === 12) { setActiveClass(classFive, activeTimeFive); }
+    else if (hours === 23) { setActiveClass(classFive, activeTimeFive); }
     else if (hours === 13) { setActiveClass(classSix, activeTimeSix); }
     else if (hours === 14) { setActiveClass(classSeven, activeTimeSeven); }
     else if (hours === 15) { setActiveClass(classEight, activeTimeEight); }
